ed3840a35f1756150c4baf688a5ab49bcc95054f
Vanesa Hernandez <vanesaherc@gmail.com>
2023-04-05 19:40:22 -0500

    Add introduction componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "react-bootstrap";
 import NavbarMenu from "./components/NavbarMenu";
+import Introduction from "./components/Introduction/Introduction";
 
 function App() {
   return (
     <div className="App">
       <NavbarMenu />
+      <Introduction />
     </div>
   );
 }
